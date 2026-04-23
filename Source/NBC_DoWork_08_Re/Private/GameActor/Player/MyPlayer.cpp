a3393ff2efa743907f545f2174e4e1935c87e5f2
gimunkwon <gimon71@gmail.com>
2026-04-23 10:49:33 +0900

    [add] Player SpringArm,CameraComponent 추가

--- Source/NBC_DoWork_08_Re/Private/GameActor/Player/MyPlayer.cpp
+++ Source/NBC_DoWork_08_Re/Private/GameActor/Player/MyPlayer.cpp
@@ -1,10 +1,21 @@
 #include "NBC_DoWork_08_Re/Public/GameActor/Player/MyPlayer.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
 AMyPlayer::AMyPlayer()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength = 600.f;
+	SpringArmComp->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComp->SetupAttachment(SpringArmComp);
+	CameraComp->bUsePawnControlRotation = false;
 }
 
 
--- Source/NBC_DoWork_08_Re/Public/GameActor/Player/MyPlayer.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Player/MyPlayer.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Character.h"
 #include "MyPlayer.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class NBC_DOWORK_08_RE_API AMyPlayer : public ACharacter
 {
@@ -13,6 +16,12 @@ public:
 	AMyPlayer();
 protected:
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera")
+	TObjectPtr<UCameraComponent> CameraComp;
+	
 public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
