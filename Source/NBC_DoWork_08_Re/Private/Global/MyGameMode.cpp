a000cb2b937f1e242e03f1654fd7c8318fdfa9aa
gimunkwon <gimon71@gmail.com>
2026-04-23 11:06:42 +0900

    [add] Gamemode에서 디폴트 플레이어 컨트롤러 클래스 추가

--- Source/NBC_DoWork_08_Re/Private/Global/MyGameMode.cpp
+++ Source/NBC_DoWork_08_Re/Private/Global/MyGameMode.cpp
@@ -1,8 +1,10 @@
 #include "NBC_DoWork_08_Re/Public/Global/MyGameMode.h"
 
 #include "NBC_DoWork_08_Re/Public/GameActor/Player/MyPlayer.h"
+#include "NBC_DoWork_08_Re/Public/GameActor/Player/Controller/MyPlayerController.h"
 
 AMyGameMode::AMyGameMode()
 {
 	DefaultPawnClass = AMyPlayer::StaticClass();
+	PlayerControllerClass = AMyPlayerController::StaticClass();
 }
