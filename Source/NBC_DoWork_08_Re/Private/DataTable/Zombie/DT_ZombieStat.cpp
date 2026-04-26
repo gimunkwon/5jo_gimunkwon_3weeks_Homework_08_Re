e8ac1d4348d88ae8083605ddf2e508b3c81d6467
gimunkwon <gimon71@gmail.com>
2026-04-26 21:17:02 +0900

    [add] 각각의 좀비들의 스탯을 관리할 DataTable 클래스 추가

---
+++ Source/NBC_DoWork_08_Re/Private/DataTable/Zombie/DT_ZombieStat.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataTable/Zombie/DT_ZombieStat.h"
+
+
+DT_ZombieStat::DT_ZombieStat()
+{
+}
+
+DT_ZombieStat::~DT_ZombieStat()
+{
+}
---
+++ Source/NBC_DoWork_08_Re/Public/DataTable/Zombie/DT_ZombieStat.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class NBC_DOWORK_08_RE_API DT_ZombieStat
+{
+public:
+	DT_ZombieStat();
+	~DT_ZombieStat();
+};
