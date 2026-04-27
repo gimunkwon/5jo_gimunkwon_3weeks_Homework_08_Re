ffd6d775fcc152d55465dd172f10c3daac4693a8
gimunkwon <gimon71@gmail.com>
2026-04-27 23:31:38 +0900

    [add] GameState 클래스 추가

---
+++ Source/NBC_DoWork_08_Re/Public/Global/MyGameState.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "MyGameState.generated.h"
+
+UCLASS()
+class NBC_DOWORK_08_RE_API AMyGameState : public AGameState
+{
+	GENERATED_BODY()
+};
