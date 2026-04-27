7e948aa5b170ccd04d8854a498ee938b039bc1d3
gimunkwon <gimon71@gmail.com>
2026-04-27 16:02:38 +0900

    [add] DrawDebugSphere로 디버그용

--- Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/AIZombieController.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/AIZombieController.h
@@ -4,6 +4,9 @@
 #include "Runtime/AIModule/Classes/AIController.h"
 #include "AIZombieController.generated.h"
 
+class UAISenseConfig_Sight;
+struct FAIStimulus;
+
 UCLASS()
 class NBC_DOWORK_08_RE_API AAIZombieController : public AAIController
 {
@@ -18,6 +21,14 @@ protected:
 	TObjectPtr<UBlackboardData> BB_Data;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AI")
 	TObjectPtr<UBehaviorTree> BT_Data;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AI")
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComp;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AI")
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+	
+	UFUNCTION()
+	void OnTargetPerceived(AActor* Actor, FAIStimulus Stimulus);
 	
 public:
 	virtual void Tick(float DeltaTime) override;
