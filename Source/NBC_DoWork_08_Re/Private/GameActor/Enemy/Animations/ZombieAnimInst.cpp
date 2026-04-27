62248e49828cfe77b3708b06aea6f55133e5e466
gimunkwon <gimon71@gmail.com>
2026-04-27 22:30:00 +0900

    [add] 좀비 애님 인스턴스 캐싱및 속도값 초기화

--- Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/Animations/ZombieAnimInst.cpp
+++ Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/Animations/ZombieAnimInst.cpp
@@ -1,4 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GameActor/Enemy/Animations/ZombieAnimInst.h"
+
+#include "GameActor/Enemy/MyZombie.h"
+
+void UZombieAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (!MyOwner)
+	{
+		MyOwner = Cast<AMyZombie>(TryGetPawnOwner());
+	}
+}
+
+void UZombieAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (MyOwner)
+	{
+		FVector Velocity = MyOwner->GetVelocity();
+		GroundSpeed = Velocity.Size2D();
+	}
+}
--- Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/Animations/ZombieAnimInst.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/Animations/ZombieAnimInst.h
@@ -1,16 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
 #include "ZombieAnimInst.generated.h"
 
-/**
- * 
- */
+class AMyZombie;
+
 UCLASS()
 class NBC_DOWORK_08_RE_API UZombieAnimInst : public UAnimInstance
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+protected:
+	UPROPERTY()
+	TObjectPtr<AMyZombie> MyOwner;
+	float GroundSpeed;
 };
