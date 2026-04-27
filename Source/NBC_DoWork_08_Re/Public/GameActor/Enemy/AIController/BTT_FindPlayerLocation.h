1ac125bb14c2f43b18720fa049b70da3cb3dcc05
gimunkwon <gimon71@gmail.com>
2026-04-27 12:33:11 +0900

    [add] Test용 TaskNode 작성

--- Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/BTT_FindPlayerLocation.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/BTT_FindPlayerLocation.h
@@ -8,4 +8,14 @@ UCLASS()
 class NBC_DOWORK_08_RE_API UBTT_FindPlayerLocation : public UBTTaskNode
 {
 	GENERATED_BODY()
+	
+public:
+	UBTT_FindPlayerLocation();
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+private:
+	UPROPERTY(EditAnywhere,Category="BlackBoard")
+	FBlackboardKeySelector TargetLocationKey;
+	UPROPERTY(EditAnywhere,Category="AI")
+	float SearchRadius;
 };
