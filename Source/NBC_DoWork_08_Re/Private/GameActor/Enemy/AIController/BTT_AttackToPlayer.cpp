70d724667b6012d6065f0021798faeeb1d5ffccf
gimunkwon <gimon71@gmail.com>
2026-04-27 16:27:35 +0900

    [add] 플레이어를 공격하는 Task 노드 추가

---
+++ Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/AIController/BTT_AttackToPlayer.cpp
@@ -0,0 +1,6 @@
+#include "GameActor/Enemy/AIController/BTT_AttackToPlayer.h"
+
+EBTNodeResult::Type UBTT_AttackToPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/BTT_AttackToPlayer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_AttackToPlayer.generated.h"
+
+UCLASS()
+class NBC_DOWORK_08_RE_API UBTT_AttackToPlayer : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
