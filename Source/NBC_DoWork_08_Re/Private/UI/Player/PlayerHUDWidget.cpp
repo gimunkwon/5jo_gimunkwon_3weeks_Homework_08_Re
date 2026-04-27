d94d3e6fc82d93d512880b26a22966708f525d33
gimunkwon <gimon71@gmail.com>
2026-04-27 11:23:41 +0900

    [add] PlayerHUD ProgressBar 업데이트 메서드 추가

--- Source/NBC_DoWork_08_Re/Private/UI/Player/PlayerHUDWidget.cpp
+++ Source/NBC_DoWork_08_Re/Private/UI/Player/PlayerHUDWidget.cpp
@@ -1,6 +1,11 @@
 #include "UI/Player/PlayerHUDWidget.h"
 
+#include "Components/ProgressBar.h"
+
 void UPlayerHUDWidget::UpdatePlayerHPBar(float MaxHP, float CurrentHP)
 {
-	
+	if (Progress_HP)
+	{
+		Progress_HP->SetPercent(CurrentHP / MaxHP);
+	}
 }
