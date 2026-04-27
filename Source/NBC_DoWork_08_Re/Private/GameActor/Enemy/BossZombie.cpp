59f45e63d676961880aa6b01300eba9e92388647
gimunkwon <gimon71@gmail.com>
2026-04-27 10:30:04 +0900

    [None]

--- Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/BossZombie.cpp
+++ Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/BossZombie.cpp
@@ -1,30 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GameActor/Enemy/BossZombie.h"
 
 
-// Sets default values
 ABossZombie::ABossZombie()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
+
 void ABossZombie::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+
 void ABossZombie::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-// Called to bind functionality to input
+
 void ABossZombie::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/BossZombie.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/BossZombie.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,17 +10,17 @@ class NBC_DOWORK_08_RE_API ABossZombie : public AMyZombie
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+	
 	ABossZombie();
 
 protected:
-	// Called when the game starts or when spawned
+	
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
+	
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
