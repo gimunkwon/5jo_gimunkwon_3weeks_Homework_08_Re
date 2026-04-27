cd97bb06eaacbad7458c658980c89e46de7f2e8e
gimunkwon <gimon71@gmail.com>
2026-04-27 21:26:19 +0900

    [Refac,fix] 기존 Beginplay쪽에서 AI감지 컴포넌트 SenseConfig가 업데이트 되지않아 Onpossess로 초기화 시점을 옮긴후 AIPerceptionComp->RequestStimuliListenerUpdate(); 호출을 통해 매니저 시스템에게 업데이트 등록 요청

--- Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/AIController/AIZombieController.cpp
+++ Source/NBC_DoWork_08_Re/Private/GameActor/Enemy/AIController/AIZombieController.cpp
@@ -1,6 +1,8 @@
 #include "GameActor/Enemy/AIController/AIZombieController.h"
 
 #include "BehaviorTree/BlackboardComponent.h"
+#include "DataTable/Zombie/DT_ZombieStat.h"
+#include "GameActor/Enemy/MyZombie.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AIPerceptionTypes.h"
 #include "Perception/AISenseConfig_Sight.h"
@@ -10,36 +12,49 @@
 AAIZombieController::AAIZombieController()
 {
 	PrimaryActorTick.bCanEverTick = true;
-	
 	AIPerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
 	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
-	
-	SightConfig->SightRadius = 700.f;
-	SightConfig->LoseSightRadius = 1000.f;
-	SightConfig->PeripheralVisionAngleDegrees = 120.f;
-	SightConfig->SetMaxAge(5.f);
-	
-	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
-	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
-	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
-	
-	AIPerceptionComp->ConfigureSense(*SightConfig);
-	AIPerceptionComp->SetDominantSense(SightConfig->GetSenseImplementation());
-	
-	AIPerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &AAIZombieController::OnTargetPerceived);
-	
 }
 
 
 void AAIZombieController::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
+
 
 void AAIZombieController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
+	
+	if (AIPerceptionComp)
+	{
+		AIPerceptionComp->Activate();
+		AIPerceptionComp->RegisterComponent();
+	}
+	
+	if (AMyZombie* MyOwner = Cast<AMyZombie>(InPawn))
+	{
+		static const FString ContextString = "InitializeAIController";
+		if (FZombieStat* ZombieStat = RowHandle.GetRow<FZombieStat>(ContextString))
+		{
+			SightConfig->SightRadius = ZombieStat->SightRadius;
+			SightConfig->LoseSightRadius = ZombieStat->LoseSightRadius;
+			SightConfig->PeripheralVisionAngleDegrees = ZombieStat->SightDegrees;
+			SightConfig->SetMaxAge(5.f);
+	
+			SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+			SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+			SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	
+			AIPerceptionComp->ConfigureSense(*SightConfig);
+			AIPerceptionComp->RequestStimuliListenerUpdate();
+			AIPerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &AAIZombieController::OnTargetPerceived);
+			UE_LOG(LogTemp,Warning,TEXT("좀비 AI컨트롤러 감지 컴포넌트 초기화 완료"));
+			
+			MyOwner->InitializeStat(RowHandle);
+		}
+	}
 	
 	if (RunBehaviorTree(BT_Data))
 	{
