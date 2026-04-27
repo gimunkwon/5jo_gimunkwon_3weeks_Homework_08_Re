f038efae57f7fa678ad649c2c3eb555177c4052c
gimunkwon <gimon71@gmail.com>
2026-04-27 22:24:48 +0900

    [add] ZombieAniminst클래스 추가

---
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/Animations/ZombieAnimInst.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ZombieAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NBC_DOWORK_08_RE_API UZombieAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+};
