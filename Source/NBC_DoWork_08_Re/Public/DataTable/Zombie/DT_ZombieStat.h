35eda14964919be6b65e8f753bfc655a2b6c58ea
gimunkwon <gimon71@gmail.com>
2026-04-27 19:24:02 +0900

    [add] 좀비 스탯 데이터 테이블 열 값 추가

--- Source/NBC_DoWork_08_Re/Public/DataTable/Zombie/DT_ZombieStat.h
+++ Source/NBC_DoWork_08_Re/Public/DataTable/Zombie/DT_ZombieStat.h
@@ -24,5 +24,16 @@ public:
 	float MaxHP;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
 	float MaxWalkSpeed;
-	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	float AttackRange;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	float AttackDamage;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	float SightRadius;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	float LoseSightRadius;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	float SightDegrees;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="ZombieStat")
+	FVector AttackBoxSize;
 };
