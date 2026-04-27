285b3dc1ea67ab05faf29c96cefe89fc1a961104
gimunkwon <gimon71@gmail.com>
2026-04-27 16:45:36 +0900

    [add] TickNode 메서드에서 블랙보드에 저장되어있는 TargetActor를 불러와 현재 AI컨트롤러에 빙의되있는 액터와의 거리를 비교해서 블랙보드에 저장되어있는 bool값을 업데이트(0.2초마다)

--- Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/UBTService_CheckAttackRange.h
+++ Source/NBC_DoWork_08_Re/Public/GameActor/Enemy/AIController/UBTService_CheckAttackRange.h
@@ -8,6 +8,11 @@ UCLASS()
 class NBC_DOWORK_08_RE_API UUBTService_CheckAttackRange : public UBTService
 {
 	GENERATED_BODY()
-	
 public:
+	UUBTService_CheckAttackRange();
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+	UPROPERTY(EditAnywhere,Category="AI")
+	float AttackRange = 150.f;
 };
