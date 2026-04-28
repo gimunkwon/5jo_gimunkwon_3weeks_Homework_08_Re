c56c3d09cc2eaced3c46942c22026beb0bbee4bc
gimunkwon <gimon71@gmail.com>
2026-04-28 23:55:17 +0900

    [add] PlyerHUD 잔탄량,현재선택한 무기 하이라이트 로직 추가

--- Source/NBC_DoWork_08_Re/Private/UI/Player/PlayerHUDWidget.cpp
+++ Source/NBC_DoWork_08_Re/Private/UI/Player/PlayerHUDWidget.cpp
@@ -1,11 +1,41 @@
 #include "UI/Player/PlayerHUDWidget.h"
 
+#include "Components/Border.h"
 #include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "GameActor/Player/MyPlayer.h"
 
 void UPlayerHUDWidget::UpdatePlayerHPBar(float MaxHP, float CurrentHP)
 {
 	if (Progress_HP)
 	{
 		Progress_HP->SetPercent(CurrentHP / MaxHP);
+	}
+}
+
+void UPlayerHUDWidget::UpdateSelectBorder(EPlayerBattleState CurrentState)
+{
+	if (Border_Bat && Border_Gun)
+	{
+		switch (CurrentState)
+		{
+		case EPlayerBattleState::Melee:
+			Border_Gun->SetBrushColor(FLinearColor::White);
+			Border_Bat->SetBrushColor(FLinearColor::Gray);
+			break;
+		case EPlayerBattleState::Gun:
+			Border_Bat->SetBrushColor(FLinearColor::White);
+			Border_Gun->SetBrushColor(FLinearColor::Gray);
+			break;
+		}
+	}
+}
+
+void UPlayerHUDWidget::UpdateAmmoText(int32 CurrentAmmo, int32 MaxAmmo)
+{
+	if (Text_GunAmmo)
+	{
+		FString AmmoText = FString::Printf(TEXT("%d / %d"),CurrentAmmo,MaxAmmo);
+		Text_GunAmmo->SetText(FText::FromString(AmmoText));
 	}
 }
