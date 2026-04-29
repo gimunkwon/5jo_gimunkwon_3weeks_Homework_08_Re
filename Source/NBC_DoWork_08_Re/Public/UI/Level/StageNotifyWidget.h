f7fbaa7d1d2b78968b6f65105a9a61ee9a568abd
gimunkwon <gimon71@gmail.com>
2026-04-29 10:26:16 +0900

    [add] Widget UtextBlock Text변경 메서드 작성

--- Source/NBC_DoWork_08_Re/Public/UI/Level/StageNotifyWidget.h
+++ Source/NBC_DoWork_08_Re/Public/UI/Level/StageNotifyWidget.h
@@ -4,8 +4,15 @@
 #include "Blueprint/UserWidget.h"
 #include "StageNotifyWidget.generated.h"
 
+class UTextBlock;
+
 UCLASS()
 class NBC_DOWORK_08_RE_API UStageNotifyWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	void UpdateStageInfoText(int32 CurrentStageIndex);
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> Text_StageInfo;
 };
