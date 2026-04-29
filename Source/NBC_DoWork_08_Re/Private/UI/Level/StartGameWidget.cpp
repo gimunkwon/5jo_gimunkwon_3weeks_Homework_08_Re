de5913fd4c87560f82e379e88164b02b0f509ddc
gimunkwon <gimon71@gmail.com>
2026-04-29 16:15:51 +0900

    [add] 게임시작 위젯 클래스 추가

---
+++ Source/NBC_DoWork_08_Re/Private/UI/Level/StartGameWidget.cpp
@@ -0,0 +1,29 @@
+#include "UI/Level/StartGameWidget.h"
+
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void UStartGameWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (Btn_StartGame)
+	{
+		Btn_StartGame->OnClicked.AddDynamic(this, &UStartGameWidget::OnStartBtn);
+	}
+	if (Btn_ExitGame)
+	{
+		Btn_ExitGame->OnClicked.AddDynamic(this, &UStartGameWidget::OnExitBtn);
+	}
+}
+
+void UStartGameWidget::OnStartBtn()
+{
+	UE_LOG(LogTemp,Warning,TEXT("스타트 버튼 눌림"));
+	UGameplayStatics::OpenLevel(GetWorld(),TEXT("L_Level1"));
+}
+
+void UStartGameWidget::OnExitBtn()
+{
+	UE_LOG(LogTemp,Warning,TEXT("게임종료 버튼 눌림"));
+}
