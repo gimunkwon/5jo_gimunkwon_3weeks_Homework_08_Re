969ebdd134a4ee1ddaae8e55232fb192e16cd068
gimunkwon <gimon71@gmail.com>
2026-04-29 11:09:38 +0900

    [add] GameOver 위젯 클래스 추가

---
+++ Source/NBC_DoWork_08_Re/Public/UI/Level/GameOverWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameOverWidget.generated.h"
+
+UCLASS()
+class NBC_DOWORK_08_RE_API UGameOverWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
